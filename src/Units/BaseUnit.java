d09970af211f24c0ef632748e62634f951d760ee
CJlaB4er-MSI <CJlaB4er@gmail.com>
2023-05-22 15:01:48 +0300

    добавленÐо условие смерти юнита

--- src/Units/BaseUnit.java
+++ src/Units/BaseUnit.java
@@ -1,13 +1,20 @@
 package Units;
 
-public class BaseUnit {
+public class BaseUnit implements GameInterface{
     protected int hp, maxHP, initiative;
     String name;
+    boolean die;
 
     public BaseUnit(int hp, int maxHP, int initiative, String name) {
         this.hp = hp;
         this.maxHP = maxHP;
         this.initiative = initiative;
         this.name = name;
+        this.die = false;
+    }
+
+    @Override
+    public void step() {
+        if (hp < 1) die = true;
     }
 }
