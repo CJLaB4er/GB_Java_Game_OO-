548b16362709aef36d2d181ef892224db03f4cff
CJlaB4er-MSI <CJlaB4er@gmail.com>
2023-05-30 00:30:40 +0300

    пехота пошла, пока только синие

--- src/Units/Melle.java
+++ src/Units/Melle.java
@@ -11,7 +11,20 @@ public abstract class Melle extends BaseUnit{
     public void step(ArrayList<BaseUnit> enemys) {
         super.step(enemys);
         BaseUnit target = findTarger(enemys);
+        if (target != null){
+            move(target);
+        }
+    }
 
+    protected void move(BaseUnit target){
+        float dx = this.coord.getCoord()[0] - target.coord.getCoord()[0];
+        float dy = this.coord.getCoord()[1] - target.coord.getCoord()[1];
+        if (dx < dy){
+            this.coord.x ++;
+        }
+        else {
+            this.coord.y ++;
+        }
 
     }
 }
